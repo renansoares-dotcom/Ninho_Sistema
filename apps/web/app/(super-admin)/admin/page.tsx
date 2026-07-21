ea863515b62006cdba277c7f78c934093270ff25
renansoares-dotcom <renansoaresgualberto@gmail.com>
2026-07-21 18:23:07 -0300

    Corrige build do Super Admin, adiciona migration 032 e landing institucional

--- apps/web/app/(super-admin)/admin/page.tsx
+++
@@ -1,5 +0,0 @@
-import EscritoriosList from "@/components/super-admin/EscritoriosList";
-
-export default function SuperAdminPage() {
-  return <EscritoriosList />;
-}
--- apps/web/app/(super-admin)/admin/usuarios/page.tsx
+++
@@ -1,5 +0,0 @@
-import UsuariosGlobalPanel from "@/components/super-admin/UsuariosGlobalPanel";
-
-export default function UsuariosGlobalPage() {
-  return <UsuariosGlobalPanel />;
-}
